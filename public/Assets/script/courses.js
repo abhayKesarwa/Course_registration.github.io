88918eb1e3305bd644dd6e32b45e7160786baeb7
Abhay Kesarwani <103197529+abhayKesarwa@users.noreply.github.com>
2022-09-15 00:07:37 +0530

    Add files via upload

---
+++ public/Assets/script/courses.js
@@ -0,0 +1,20 @@
+function display1(){
+    document.getElementById("detail1").style.display="block";
+    document.getElementById("detail2").style.display="none";
+    document.getElementById("detail3").style.display="none";
+
+}
+
+function display2(){
+    document.getElementById("detail1").style.display="none";
+    document.getElementById("detail2").style.display="block";
+    document.getElementById("detail3").style.display="none";
+
+}
+
+function display3(){
+    document.getElementById("detail1").style.display="none";
+    document.getElementById("detail2").style.display="none";
+    document.getElementById("detail3").style.display="block";
+
+}
